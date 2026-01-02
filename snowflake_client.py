ca96ef1d74a9140863b91e11cbf43503cadbdabc
Abhay  Dubey <146931800+abhaydubey200@users.noreply.github.com>
2026-01-02 13:22:26 +0530

    Update snowflake_client.py

--- snowflake_client.py
+++ snowflake_client.py
@@ -2,11 +2,18 @@ import snowflake.connector
 
 def get_snowflake_connection():
     return snowflake.connector.connect(
-        user="ABHAY2004",
-        password="Abhay@7505991639",
-        account="VKOZIAJ-KC24613",
-        warehouse="COMPANY_WH",
-        database="BIZOM_DB",
-        schema="OUTLET_SCHEMA",
-        role="PUBLIC"
+       user = "ABHAY2004"
+        password = "Abhay@7505991639"
+        account = "VKOZIAJ-KC24613"
+        warehouse = "YOUR_WAREHOUSE"
+        database = "FMCG_DWH"
+        schema = "RAW"
     )
+
+
+
+
+
+
+
+
